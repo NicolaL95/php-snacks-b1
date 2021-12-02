3d427d9731d3475669b3a6f04b1bcaeb61b3b492
NicolaL95 <nicola.lilla@gmail.com>
2021-12-02 11:44:38 +0100

    Fine lavorazione

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+$match = array(
+    array("Milano","Piacenza",103,97),
+     array("Torino","Livorno",73,82),
+      array("Palermo","Bari",88,88),
+       array("Cremona","Firenze",111,99),
+);
+
+for ($x = 0; $x < count($match); $x++) {
+  echo $match[$x][0], ' - ', $match[$x][1], ' | ', $match[$x][2] ,' - ', $match[$x][3], "<br>";
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
